3aa53616f733ddd6e4f84017bbc3a49e41657a2e
Fernando Daumichen <fernando.daumichen@gmail.com>
2024-04-16 22:48:21 -0700

    functional db and form

--- stacks/MyStack.ts
+++ stacks/MyStack.ts
@@ -3,6 +3,13 @@ import { StackContext, Api , StaticSite} from "sst/constructs";
 export function API({ stack }: StackContext) {
 
   const api = new Api(stack, "api", {
+    defaults: {
+      function: {
+        environment: {
+          DRIZZLE_DATABASE_URL: process.env.DRIZZLE_DATABASE_URL!,
+        },
+      },
+    },
     routes: {
       "GET /": "packages/functions/src/lambda.handler",
       "GET /all-jobs": 'packages/functions/src/all-jobs.handler',
